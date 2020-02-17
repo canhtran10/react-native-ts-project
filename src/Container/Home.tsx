60f66aecd723aa8cf3cdb029ab903cc805596f1d
Canh Tran <quangcanh.dhv@gmail.com>
2020-02-17 13:29:31 +0700

    - version 0.1

--- src/Container/Home.tsx
+++ src/Container/Home.tsx
@@ -1,9 +1,10 @@
-import {Button, Text, View} from "react-native";
+import {AsyncStorage, Button, Text, View} from "react-native";
 import * as React from "react";
 import {stylesGlobal} from '../Layout';
-import {Component} from "react";
+import {Component, useEffect} from "react";
 import {connect} from "react-redux";
 import {increment, decrement} from "../Store/Action";
+import messaging from '@react-native-firebase/messaging';
 
 export interface Props {
   navigation: any,
@@ -19,8 +20,44 @@ class _Home extends Component<Props>{
     }
   }
 
-  componentDidMount(): void {
+  async registerAppWithFCM() {
+    await messaging().registerForRemoteNotifications();
+  }
+
+  async requestPermission() {
+    const granted = messaging().requestPermission();
+
+    if (granted) {
+      console.log('User granted messaging permissions!');
+    } else {
+      console.log('User declined messaging permissions :(');
+    }
+  }
+
+  App() {
+    useEffect(() => {
+      const unsubscribe = messaging().onMessage(async remoteMessage => {
+        console.log('FCM Message Data:', remoteMessage.data);
+
+        // Update a users messages list using AsyncStorage
+        const currentMessages = await AsyncStorage.getItem('messages');
+        const messageArray = JSON.parse(currentMessages);
+        messageArray.push(remoteMessage.data);
+        await AsyncStorage.setItem('messages', JSON.stringify(messageArray));
+      });
+    }, []);
+  }
+
+  async componentDidMount() {
     this.props.onIncrement(1);
+
+    console.log(await messaging().getToken());
+
+    this.App();
+
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+    });
   }
 
   render() {
